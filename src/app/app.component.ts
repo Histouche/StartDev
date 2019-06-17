d38a7d0374c25f3b1c39af86754fe98b4bef9e4a
Jonathan <venianelam@gmail.com>
2019-06-17 16:28:00 +0200

    désactivation connexion auto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-root',
@@ -11,16 +12,17 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
 
-  connecte = true;
+  connecte = false;
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private router: Router
+    private router: Router,
+    public storage: Storage
   ) {
     this.initializeApp();
-    if (this.connecte === false) {
+    if (this.connecte === false  ) {
         this.router.navigate(['/connexion']);
     }
       /* this.authService.loggedIn().then((loggedIn) => {
